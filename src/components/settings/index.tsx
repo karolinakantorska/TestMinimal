0520d86b4d3757b81e3bfcdf4edf419bab20dc92
karolina <karolina.kantorska@gmail.com>
2022-09-07 09:32:26 +0200

    drawer removed

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -1,5 +1,3 @@
-import SettingsDrawer from './drawer';
-//
 import ThemeContrast from './ThemeContrast';
 import ThemeRtlLayout from './ThemeRtlLayout';
 import ThemeColorPresets from './ThemeColorPresets';
@@ -16,7 +14,6 @@ export default function ThemeSettings({ children }: Props) {
       <ThemeContrast>
         <ThemeRtlLayout>
           {children}
-          <SettingsDrawer />
         </ThemeRtlLayout>
       </ThemeContrast>
     </ThemeColorPresets>
